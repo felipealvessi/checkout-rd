2e61cf67de05985bab78ec64d675cbc7015bd4c5
felipealvessi <felipealvesinfor@mail.com>
2020-10-02 10:15:32 -0300

    api Cep function added

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const inputCep = document.querySelector("#cepUsuario");
 
 inputCep.addEventListener("keyup", apiCEP);
 
+
 function apiCEP(e) {
   if (this.value.length == 8) {
     alert('CEP digitado: ' + this.value);

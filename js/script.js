841b9d29329c740d246a7cbb79bfde91eca920aa
felipealvessi <felipealvesinfor@mail.com>
2020-10-02 10:14:43 -0300

    includ function apiCEP

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+console.log("Projeto Checkout");
+const inputCep = document.querySelector("#cepUsuario");
 
+inputCep.addEventListener("keyup", apiCEP);
+
+function apiCEP(e) {
+  if (this.value.length == 8) {
+    alert('CEP digitado: ' + this.value);
+  }
+}
